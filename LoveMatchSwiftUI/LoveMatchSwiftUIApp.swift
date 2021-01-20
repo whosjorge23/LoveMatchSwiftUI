2bc16ccb855676317ed7fa8f73caf80325fa6f5b
Giorgio Giannotta <jorge_upa@msn.com>
2021-01-20 18:24:10 +0100

    Initial Commit

---
+++ LoveMatchSwiftUI/LoveMatchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveMatchSwiftUIApp.swift
+//  LoveMatchSwiftUI
+//
+//  Created by Jorge Giannotta on 20/01/21.
+//
+
+import SwiftUI
+
+@main
+struct LoveMatchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
